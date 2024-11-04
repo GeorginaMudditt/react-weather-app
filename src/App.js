2a9e2962ad1bb8c6f9994c7a111c3e46757876f4
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-11-04 16:00:39 +0100

    add footer with links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Search from "./Search.js";
+import Footer from "./Footer.js";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was code by{" "}
+      <a
+        href="https://www.linkedin.com/in/georgina-mudditt/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Georgina Mudditt
+      </a>
+      , is open-sourced on{" "}
+      <a
+        href="https://github.com/GeorginaMudditt/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+      , and is hosted on{" "}
+      <a
+        href="https://gm-react-weather-app.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+      .
+    </p>
+  );
+}
