9003f31bd5f89055dcfeb3377bcf3c3f8900b75a
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-11-05 17:35:52 +0100

    install moment to update date/time in real time

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
+import moment from "moment";
 
 export default function Header() {
+  let today = moment().format("dddd D MMMM HH:mm");
   return (
     <div className="Header">
       <h1>World Weather</h1>
-      <h2 className="date">Tuesday 5 November 11:27</h2>
+      <h2 className="date">{today}</h2>
       <h3 className="tagline">
         Search for the current and forecasted weather conditions
         <br /> for any city in the world
